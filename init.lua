b541c1c8598ed0171b3aad109248953f766c507d
fctofani <fctofani2@gmail.com>
2024-10-27 14:36:23 -0300

    initial setup

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("fctofani")
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
---
+++ lua/fctofani/init.lua
@@ -0,0 +1,6 @@
+require("fctofani.remap")
+require("fctofani.packer")
+vim.cmd[[colorscheme dracula]]
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
