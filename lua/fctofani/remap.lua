622f1fc2e58cf397d3e3fbe4ae15f6b4823a1947
Fabrício César Tofani <fabricio.tofani@cassotis.com>
2024-10-28 14:37:34 -0300

    add treesiter and nvimtree

--- lua/fctofani/remap.lua
+++ lua/fctofani/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<leader>n", vim.cmd.noh)
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
 
