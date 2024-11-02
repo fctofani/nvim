1a7e84953b592f3de90a3e2ae782ef9b493138e5
fctofani <fctofani2@gmail.com>
2024-11-02 15:29:01 -0300

    add rust-analyzer config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,6 +4,13 @@ return {
 	dependencies = {
 		"mason.nvim",
 		{ "williamboman/mason-lspconfig.nvim", config = function() end },
-	}
+	},
+	config = function () 
+		local lspconfig = require("lspconfig")
+		local mason = require("mason")
+		mason.setup()
+
+		lspconfig.rust_analyzer.setup({})
+	end,
 }
 
